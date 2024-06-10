e612c15ce2090052b431610676727f8b20473a12
aaaravv <aaravmishra619@gmail.com>
2024-06-10 12:13:44 +0530

    feat: Add method to `FCM` service for sending asynchronous notification
    Introduce async method for sending Firebase Cloud Messaging notifications to improve responsiveness and efficiency. Utilizes CompletableFuture and @Async annotation. Enhances user experience in real-time notification delivery.

--- src/main/java/com/ripplereach/ripplereach/services/FCMService.java
+++ src/main/java/com/ripplereach/ripplereach/services/FCMService.java
@@ -1,5 +1,8 @@
 package com.ripplereach.ripplereach.services;
 
+import java.util.concurrent.CompletableFuture;
+
 public interface FCMService {
     void sendNotification(String targetToken, String title, String body);
+    CompletableFuture<Void> sendNotificationAsync(String targetToken, String title, String body);
 }
--- src/main/java/com/ripplereach/ripplereach/services/impl/FCMServiceImpl.java
+++ src/main/java/com/ripplereach/ripplereach/services/impl/FCMServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ripplereach.ripplereach.services.impl;
 
+import com.google.api.core.ApiFuture;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import com.ripplereach.ripplereach.services.FCMService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @Slf4j
@@ -26,9 +31,33 @@ public class FCMServiceImpl implements FCMService {
 
         try {
             String response = FirebaseMessaging.getInstance().send(message);
-            System.out.println("Successfully sent message: " + response);
+            log.info("Successfully sent fcm message: {}", response);
         } catch (FirebaseMessagingException e) {
             log.error("Error while sending notification", e);
         }
+    }
+
+    @Override
+    @Async
+    public CompletableFuture<Void> sendNotificationAsync(String targetToken, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        Message message = Message.builder()
+                .setToken(targetToken)
+                .setNotification(notification)
+                .build();
+
+        ApiFuture<String> response = FirebaseMessaging.getInstance().sendAsync(message);
+        return CompletableFuture.runAsync(() -> {
+            try {
+                String result = response.get();
+                log.info("Successfully sent FCM message: {}", result);
+            } catch (InterruptedException | ExecutionException e) {
+                log.error("Error while getting response from Firebase", e);
+            }
+        });
     }
 }
