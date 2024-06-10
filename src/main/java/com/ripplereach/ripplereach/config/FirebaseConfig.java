2db2becd116e940662b79ad174de389542440d4e
aaaravv <aaravmishra619@gmail.com>
2024-06-10 14:10:19 +0530

    chore: Update `firebase` configuration setup
    Introduce dynamic path for Firebase service account key This fixes the issue in production where the service account key wasn't being picked by file input stream inside the docker container due to invalid path reference.

--- src/main/java/com/ripplereach/ripplereach/config/FirebaseConfig.java
+++ src/main/java/com/ripplereach/ripplereach/config/FirebaseConfig.java
@@ -4,25 +4,34 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.admin.sdk.json.path}")
+    private String firebaseAdminSdkPath;
+
     private static boolean isInstanceExists = false;
+
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/ripplereach-firebase-adminsdk.json");
+            FileInputStream serviceAccount = new FileInputStream(firebaseAdminSdkPath);
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
