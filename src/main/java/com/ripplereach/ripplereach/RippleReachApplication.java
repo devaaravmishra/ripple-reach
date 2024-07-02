c2f8f46d756c0168f14c88b2a0833cdd82fc7860
aaaravv <aaravmishra619@gmail.com>
2024-07-02 17:20:50 +0530

    feat: Enable `spring-data-web-support` for page serialization
    Enable Spring Data web support for page serialization at the global level. This change allows for more efficient handling of paginated data by enabling the serialization of pages via DTOs.

--- src/main/java/com/ripplereach/ripplereach/RippleReachApplication.java
+++ src/main/java/com/ripplereach/ripplereach/RippleReachApplication.java
@@ -4,10 +4,12 @@ import com.ripplereach.ripplereach.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableAsync
 @Import(OpenAPIConfiguration.class)
 public class RippleReachApplication {
