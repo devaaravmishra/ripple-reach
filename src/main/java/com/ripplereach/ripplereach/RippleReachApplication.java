aef464dec42fb9863c95aae4bb5fb2291104cdbb
aaaravv <aaravmishra619@gmail.com>
2024-07-20 19:22:07 +0530

    refactor: Enable caching in RippleReachApplication
    Enable caching in the RippleReachApplication class by adding the @EnableCaching annotation. This change improves performance by caching frequently accessed data.

--- src/main/java/com/ripplereach/ripplereach/RippleReachApplication.java
+++ src/main/java/com/ripplereach/ripplereach/RippleReachApplication.java
@@ -3,6 +3,7 @@ package com.ripplereach.ripplereach;
 import com.ripplereach.ripplereach.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,9 +12,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableSpringDataWebSupport(
     pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableAsync
+@EnableCaching
 @Import(OpenAPIConfiguration.class)
 public class RippleReachApplication {
   public static void main(String[] args) {
     SpringApplication.run(RippleReachApplication.class, args);
   }
+
+  //  @Bean
+  //  public CommandLineRunner run(MigrationService migrationService) {
+  //    return args -> {
+  //      migrationService.migrateImages("src/main/resources/static/images/", "images/");
+  //    };
+  //  }
 }
