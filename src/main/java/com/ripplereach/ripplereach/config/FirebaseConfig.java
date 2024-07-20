60bff0705b6626de55d03bc834f46308e72cd606
aaaravv <aaravmishra619@gmail.com>
2024-07-20 18:28:13 +0530

    chore: Update FirebaseConfig to use resourceLoader for FirebaseOptions & add storage bucket config
    Update the FirebaseConfig class to use the resourceLoader to load the ripplereach-firebase-adminsdk.json file instead of using a FileInputStream. This change ensures better resource handling and compatibility with the latest codebase. Additionally, set the storage bucket value from the application.properties file.

--- src/main/java/com/ripplereach/ripplereach/config/FirebaseConfig.java
+++ src/main/java/com/ripplereach/ripplereach/config/FirebaseConfig.java
@@ -4,30 +4,38 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
 import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 @Configuration
 public class FirebaseConfig {
 
-  @Value("${firebase.admin.sdk.json.path}")
-  private String firebaseAdminSdkPath;
-
   private static boolean isInstanceExists = false;
+
+  @Autowired ResourceLoader resourceLoader;
+
+  @Value("${storage.bucket}")
+  private String storageBucket;
 
   @PostConstruct
   public void initialize() {
     try {
-      FileInputStream serviceAccount = new FileInputStream(firebaseAdminSdkPath);
+      Resource resource =
+          resourceLoader.getResource("classpath:ripplereach-firebase-adminsdk.json");
+      InputStream serviceAccountStream = resource.getInputStream();
 
       FirebaseOptions options =
           FirebaseOptions.builder()
-              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+              .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
+              .setStorageBucket(storageBucket)
               .build();
 
       List<FirebaseApp> firebaseApps = FirebaseApp.getApps();
